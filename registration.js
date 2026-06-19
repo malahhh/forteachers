540f6c478c17fb67d3d1be3256ebea8c004a1c63
malahhh <vradosikmain@gmail.com>
2026-06-19 17:17:10 +0300

    1.0.0 v

--- registration.js
+++ registration.js
@@ -1,3 +1,7 @@
+const button = document.querySelector("#regButton");
+
+button.addEventListener("click", runScriptRegistration);
+
 function registration(name, age, frequency){
   console.log("Регистрация начата", name, age, frequency)
   return {
@@ -7,8 +11,17 @@ function registration(name, age, frequency){
   }
 }
 
+// Запрос данных о пользователе в функции runScriptRegistration потому что иначе не будет слушать кнопку
+function runScriptRegistration(){
+  let name = prompt("Ваше имя?")
+  let age = prompt("Ваш возраст?")
+  let frequency = prompt("Как часто хотите заниматься? Напишите количество раз в неделю. Например, 2")
 
-name = prompt("Ваше имя?")
-age = prompt("Ваш возраст?")
-frequency = prompt("Как часто хотите заниматься? Напишите количество раз в неделю. Например, 2")
-let newUser = registration(name, age, frequency)
+  // Регистрация нового юзера по модели
+  let newUser = registration(name, age, frequency)
+
+  alert(`Новый пользователь: ${newUser.name}, возраст: ${newUser.age}`);
+
+}
+
+
